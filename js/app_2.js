607db1577f69b409d5b1b37216ed974329e33768
Dima <dimakurinnoy8@gmail.com>
2022-07-30 15:56:20 +0200

    animatet js test 3

---
+++ js/app_2.js
@@ -0,0 +1,7 @@
+import PhotoSwipeLightbox from '../js/photoswipe-lightbox.esm.min.js';
+    const lightbox = new PhotoSwipeLightbox({
+      gallery: '#gallery--getting-started',
+      children: 'a',
+      pswpModule: () => import('../js/photoswipe.esm.min.js')
+    });
+    lightbox.init();
